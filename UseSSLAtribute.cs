cf4b0663bf915571ad6ab8a522ddd8e7cebb31ec
AtoMaso <srbinovskimirko@gmail.com>
2018-01-14 21:37:25 +1000

    Add files via upload

--- UseSSLAtribute.cs
+++ UseSSLAtribute.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Filters;
 
-namespace WebApiWhisperer
+namespace iTradeWebApi
 {
      public class UseSSLAttribute : AuthorizationFilterAttribute
      {
