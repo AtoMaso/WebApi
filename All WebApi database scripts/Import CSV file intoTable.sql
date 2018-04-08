bbebc3b3318401843c1d3b5922c5fb2480e137ec
AtoMaso <srbinovskimirko@gmail.com>
2018-04-08 21:22:46 +1000

    Delete Import CSV file intoTable.sql

--- All WebApi database scripts/Import CSV file intoTable.sql
+++
@@ -1,5 +0,0 @@
-use iTradeDatabase
-bulk insert [dbo].StatesPostcodesSuburbs
-from 'C:\Development\Angular 2\StatesCitiesSuburbsPostcodes\Australian_StatesPostcodesSuburbs.csv'
-with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
-go
