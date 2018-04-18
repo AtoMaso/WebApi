54b9c15a9ad6ecbed4cd1b8919d6d44df6fafc09
AtoMaso <srbinovskimirko@gmail.com>
2018-04-18 16:38:33 +1000

    Add files via upload

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -74,17 +74,6 @@ namespace WebApi.Controllers
         }
 
 
-        //public IHttpActionResult ManageAccount(string id)
-        //{
-        //    if (!string.IsNullOrEmpty(id))
-        //    {
-        //        string page = "~/html/" + id + ".html";
-        //        return new FilePathResult(page, "text/html");
-        //    }
-        //    return new FilePathResult("~/html/login.html", "text/html");
-        //}
-
-
         // GET api/account/UserInfo
         [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
         [Route("UserInfo")]
@@ -460,39 +449,6 @@ namespace WebApi.Controllers
             return true;
         }
 
-
-        //public void Update(User userParam, string password = null)
-        //{
-        //    var user = _context.Users.Find(userParam.Id);
-
-        //    if (user == null)
-        //        throw new AppException("User not found");
-
-        //    if (userParam.Username != user.Username)
-        //    {
-        //        // username has changed so check if the new username is already taken
-        //        if (_context.Users.Any(x => x.Username == userParam.Username))
-        //            throw new AppException("Username " + userParam.Username + " is already taken");
-        //    }
-
-        //    // update user properties
-        //    user.FirstName = userParam.FirstName;
-        //    user.LastName = userParam.LastName;
-        //    user.Username = userParam.Username;
-
-        //    // update password if it was entered
-        //    if (!string.IsNullOrWhiteSpace(password))
-        //    {
-        //        byte[] passwordHash, passwordSalt;
-        //        CreatePasswordHash(password, out passwordHash, out passwordSalt);
-
-        //        user.PasswordHash = passwordHash;
-        //        user.PasswordSalt = passwordSalt;
-        //    }
-
-        //    _context.Users.Update(user);
-        //    _context.SaveChanges();
-        //}
 
 
         #region Helpers
